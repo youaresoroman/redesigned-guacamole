d0429159852967e26e7dfd44562c94a15d71117d
Roman Yankowski <romanjankowski@groupoffice.ch>
2021-09-27 09:35:20 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Test project in React JS with Cloudflare Workers API
+
+[Preview link](https://redisigned-guacomole.nemcsygimt.workers.dev/)
--- packages/site/src/components/Layout/Content/__testz__/Component-test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Component from '..';
-
-test('renders Test Component', () => {
-  render(<Component />);
-  const linkElement = screen.getByText(/Test Component/i);
-  expect(linkElement).toBeInTheDocument();
-});
